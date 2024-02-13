8021ed61cbfbac372070af5ce365b496a72a5ad9
willemml <willem@leit.so>
2024-02-13 12:46:57 -0800

    Adds script to compile and flash from CLI

---
+++ compile_and_flash.sh
@@ -0,0 +1,10 @@
+# generate project files
+quartus_sh -t project.tcl
+
+# compile project
+quartus_map project
+quartus_fit project
+quartus_asm project
+
+# program
+quartus_pgm -m JTAG -o "s;SOCVHPS@1" -o "p;project.sof;5CSEMA5F31@2"
