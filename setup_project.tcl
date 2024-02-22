afcc3f0fd2271b78f90504ca372c8262b22d7d40
willemml <willem@leit.so>
2024-02-22 09:53:07 -0800

    Adds more pin definitions

--- setup_project.tcl
+++ setup_project.tcl
@@ -7,27 +7,81 @@ set_global_assignment -name VHDL_FILE toplevel.vhd
 set_global_assignment -name TOP_LEVEL_ENTITY toplevel
 
 # Assign pins
-set_location_assignment -to switches[9] PIN_AE12
-set_location_assignment -to switches[8] PIN_AD10
-set_location_assignment -to switches[7] PIN_AC9
-set_location_assignment -to switches[6] PIN_AE11
-set_location_assignment -to switches[5] PIN_AD12
-set_location_assignment -to switches[4] PIN_AD11
-set_location_assignment -to switches[3] PIN_AF10
-set_location_assignment -to switches[2] PIN_AF9
-set_location_assignment -to switches[1] PIN_AC12
-set_location_assignment -to switches[0] PIN_AB12
+set_location_assignment -to CLOCK_50MHZ PIN_AA16
 
-set_location_assignment -to leds[9] PIN_Y21
-set_location_assignment -to leds[8] PIN_W21
-set_location_assignment -to leds[7] PIN_W20
-set_location_assignment -to leds[6] PIN_Y19
-set_location_assignment -to leds[5] PIN_W19
-set_location_assignment -to leds[4] PIN_W17
-set_location_assignment -to leds[3] PIN_V18
-set_location_assignment -to leds[2] PIN_V17
-set_location_assignment -to leds[1] PIN_W16
-set_location_assignment -to leds[0] PIN_V16
+set_location_assignment -to SW[9] PIN_AE12
+set_location_assignment -to SW[8] PIN_AD10
+set_location_assignment -to SW[7] PIN_AC9
+set_location_assignment -to SW[6] PIN_AE11
+set_location_assignment -to SW[5] PIN_AD12
+set_location_assignment -to SW[4] PIN_AD11
+set_location_assignment -to SW[3] PIN_AF10
+set_location_assignment -to SW[2] PIN_AF9
+set_location_assignment -to SW[1] PIN_AC12
+set_location_assignment -to SW[0] PIN_AB12
 
+set_location_assignment -to LEDR[9] PIN_Y21
+set_location_assignment -to LEDR[8] PIN_W21
+set_location_assignment -to LEDR[7] PIN_W20
+set_location_assignment -to LEDR[6] PIN_Y19
+set_location_assignment -to LEDR[5] PIN_W19
+set_location_assignment -to LEDR[4] PIN_W17
+set_location_assignment -to LEDR[3] PIN_V18
+set_location_assignment -to LEDR[2] PIN_V17
+set_location_assignment -to LEDR[1] PIN_W16
+set_location_assignment -to LEDR[0] PIN_V16
+
+set_location_assignment -to KEY[0] PIN_AA14
+set_location_assignment -to KEY[1] PIN_AA15
+set_location_assignment -to KEY[2] PIN_W15
+set_location_assignment -to KEY[3] PIN_Y16
+
+set_location_assignment -to HEX0[0] PIN_AE26
+set_location_assignment -to HEX0[1] PIN_AE27
+set_location_assignment -to HEX0[2] PIN_AE28
+set_location_assignment -to HEX0[3] PIN_AG27
+set_location_assignment -to HEX0[4] PIN_AF28
+set_location_assignment -to HEX0[5] PIN_AG28
+set_location_assignment -to HEX0[6] PIN_AH28
+
+set_location_assignment -to HEX1[0] PIN_AJ29
+set_location_assignment -to HEX1[1] PIN_AH29
+set_location_assignment -to HEX1[2] PIN_AH30
+set_location_assignment -to HEX1[3] PIN_AG30
+set_location_assignment -to HEX1[4] PIN_AF29
+set_location_assignment -to HEX1[5] PIN_AF30
+set_location_assignment -to HEX1[6] PIN_AD27
+
+set_location_assignment -to HEX2[0] PIN_AB23
+set_location_assignment -to HEX2[1] PIN_AE29
+set_location_assignment -to HEX2[2] PIN_AD29
+set_location_assignment -to HEX2[3] PIN_AC28
+set_location_assignment -to HEX2[4] PIN_AD30
+set_location_assignment -to HEX2[5] PIN_AC29
+set_location_assignment -to HEX2[6] PIN_AC30
+
+set_location_assignment -to HEX3[0] PIN_AD26
+set_location_assignment -to HEX3[1] PIN_AC27
+set_location_assignment -to HEX3[2] PIN_AD25
+set_location_assignment -to HEX3[3] PIN_AC25
+set_location_assignment -to HEX3[4] PIN_AB28
+set_location_assignment -to HEX3[5] PIN_AB25
+set_location_assignment -to HEX3[6] PIN_AB22
+
+set_location_assignment -to HEX4[0] PIN_AA24
+set_location_assignment -to HEX4[1] PIN_Y23
+set_location_assignment -to HEX4[2] PIN_Y24
+set_location_assignment -to HEX4[3] PIN_W22
+set_location_assignment -to HEX4[4] PIN_W24
+set_location_assignment -to HEX4[5] PIN_V23
+set_location_assignment -to HEX4[6] PIN_W25
+
+set_location_assignment -to HEX5[0] PIN_V25
+set_location_assignment -to HEX5[1] PIN_AA28
+set_location_assignment -to HEX5[2] PIN_Y27
+set_location_assignment -to HEX5[3] PIN_AB27
+set_location_assignment -to HEX5[4] PIN_AB26
+set_location_assignment -to HEX5[5] PIN_AA26
+set_location_assignment -to HEX5[6] PIN_AA25
 
 project_close
--- toplevel.vhd
+++ toplevel.vhd
@@ -2,11 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity toplevel is port (
-  switches : in std_logic_vector(9 downto 0);
-  leds : out std_logic_vector(9 downto 0)
+  CLOCK_50MHZ : in std_logic;
+  SW : in std_logic_vector(9 downto 0);
+  KEY : in std_logic_vector(3 downto 0);
+  LEDR : out std_logic_vector(9 downto 0);
+  HEX0 : out std_logic_vector(6 downto 0);
+  HEX1 : out std_logic_vector(6 downto 0);
+  HEX2 : out std_logic_vector(6 downto 0);
+  HEX3 : out std_logic_vector(6 downto 0);
+  HEX4 : out std_logic_vector(6 downto 0);
+  HEX5 : out std_logic_vector(6 downto 0)
 );
 end entity;
 
 architecture sys of toplevel is begin
-leds <= switches;
+  LEDR <= SW;
 end architecture;
